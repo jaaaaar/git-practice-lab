a306e0af6492a34ea716776d1990c420c2e0e381
jaaaaar <roomet2006@gmail.com>
2025-09-19 06:30:37 +0000

    Muuda väljund lühemaks

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ def subtract(a, b):
     return a - b
 
 if __name__ == "__main__":
-    print("Calculator: 2 + 3 =", add(2, 3))
+    print("CALC RESULT: 2 + 3 =", add(2, 3))
 
 def multiply(a, b):
     return a * b
